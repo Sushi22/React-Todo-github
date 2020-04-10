b5232fff55b1407ec87ab24d7fc00233a69dff21
Sushi22 <sushmitajoshi22@gmail.com>
2020-04-10 14:10:37 +0530

    Updates

---
+++ precache-manifest.d28bf0b26c814fe610af2222c9d009b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86212a872ea9af2c9dc51dc3d053a6f8",
+    "url": "/React-Todo-github/index.html"
+  },
+  {
+    "revision": "76deaa65e6f8b9904676",
+    "url": "/React-Todo-github/static/css/main.f1bc4ba2.chunk.css"
+  },
+  {
+    "revision": "273410781497198085b2",
+    "url": "/React-Todo-github/static/js/2.85c3c5d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-github/static/js/2.85c3c5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76deaa65e6f8b9904676",
+    "url": "/React-Todo-github/static/js/main.2f6f3463.chunk.js"
+  },
+  {
+    "revision": "a49a5aac3fb141ab4a57",
+    "url": "/React-Todo-github/static/js/runtime-main.b05a81d2.js"
+  }
+]);
